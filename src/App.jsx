4b863be212e2d667b0bc6b65cae1a2617c435b92
Abhay verma <abhaychhani@gmail.com>
2025-04-03 08:52:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(Math.random > 0.5 ? true : false);
+
+  const handleClick = (index) => {
+    if (board[index] || checkWins(board) || isDraw(board)) return;
+    const newBoard = board.slice();
+    newBoard[index] = isXNext ? "X" : "O";
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const checkWins = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let [a, b, c] of lines) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  };
+
+  const isDraw = (board) => {
+    return board.every((box) => box !== null) && !checkWins(board);
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setIsXNext(Math.random > 0.5 ? true : false);
+  };
+
+  const winner = checkWins(board);
+  const draw = isDraw(board);
+  const status = winner
+    ? `Winner is ${winner}`
+    : draw
+    ? "It's a draw"
+    : `${isXNext ? "X" : "O"} Player Move`;
+
+  const borderStyle = [
+    "border-r-4 border-b-4",
+    "border-x-4 border-b-4",
+    "border-l-4 border-b-4",
+    "border-y-4 border-r-4",
+    "border-4",
+    "border-y-4 border-l-4",
+    "border-t-4 border-r-4",
+    "border-x-4 border-t-4",
+    "border-l-4 border-t-4",
+  ];
+  return (
+    <>
+      <div className="grid grid-cols-3 grid-rows-3 h-[450px] aspect-square mx-auto my-6">
+        {board.map((value, index) => (
+          <div
+            className={`${borderStyle[index]} border-sky-500 font-mono font-bold text-5xl flex items-center justify-center`}
+            key={index}
+            onClick={() => {
+              handleClick(index);
+            }}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+      <button className="bg-blue-500 px-6 py-1 rounded mx-auto text-white cursor-pointer block " onClick={resetGame}>
+        reset
+      </button>
+      <span className=" block text-center py-4 ">{status}</span>
+
+    </>
+  );
+}
+
+export default App;
